5b922f12c8aaacee1ee72845dd2f23327bef9876
Chris Mony <chmony@student.ethz.ch>
2021-03-09 20:36:45 +0100

    Cleaned up ERAI modelling notebook

--- src/02_analysis/model_training.py
+++ src/02_analysis/model_training.py
@@ -5,53 +5,70 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def fit_model_on_all_ERA_features_with_CV_and_return_most_important_features(model, parameters_CV, df_ERA, feature_names, train_mask, test_mask, with_CV):
-    '''
+
+def fit_model_and_obtain_importances(model, df, feature_names, train_mask, test_mask, with_CV=False, parameters_CV=[]):
+    """
     Train ML model on whole ERA data and return the most important features. Allows to train with or without CV.
-    '''
-    print("Started")
+    @param model: Any classification model that offers fit and predict_proba functions
+    @param df: Dataframe with features and target ("Foehn")
+    @param feature_names: Feature names
+    @param train_mask: Mask for the training set
+    @param test_mask: Mask for the test set
+    @param with_CV: Whether to fit with CV
+    @param parameters_CV: Which parameters to use for CV (dict)
+    @return: Dataframe with sorted feature importances
+    """
+
+    print("Started fitting ...")
     
-    if with_CV==True:    
-        model_CV = GridSearchCV(model, parameters_CV, cv=3, n_jobs=5, scoring='neg_log_loss', verbose=3)
-        model_CV.fit(df_ERA.loc[train_mask, feature_names], 
-                     df_ERA.loc[train_mask, "Foehn"])
+    if with_CV == True:  # Fit with cross-validation
+        model_CV = GridSearchCV(model, parameters_CV, cv=5, n_jobs=5, scoring='neg_log_loss', verbose=3)
+        model_CV.fit(df.loc[train_mask, feature_names],
+                     df.loc[train_mask, "Foehn"])
         
         print(model_CV.best_params_)
         best_model = model_CV.best_estimator_
-    else:
-        model.fit(df_ERA.loc[train_mask, feature_names], 
-                 df_ERA.loc[train_mask, "Foehn"])
+    else:  # Just fit a model normally
+        model.fit(df.loc[train_mask, feature_names],
+                  df.loc[train_mask, "Foehn"])
         best_model=model
+
+    print("Finished fitting ...")
     
     # Make predictions
-    prediction_probas = best_model.predict_proba(df_ERA.loc[test_mask, feature_names])
+    prediction_probas = best_model.predict_proba(df.loc[:, feature_names])[:, 1]
     
-    # Calculate best threshold
-    precisions, recalls, thresholds = precision_recall_curve(df_ERA.loc[test_mask, "Foehn"], prediction_probas[:,1])
+    # Calculate best threshold on train set
+    precisions, recalls, thresholds = precision_recall_curve(df.loc[train_mask, "Foehn"], prediction_probas[train_mask])
     best_threshold = thresholds[np.argmin(abs(precisions-recalls))-1]
-    predictions = (prediction_probas[:,1]>best_threshold).astype(int)
+    predictions = (prediction_probas > best_threshold).astype(int)
     
     # Plot precision and recall curves
+    precisions, recalls, thresholds = precision_recall_curve(df.loc[test_mask, "Foehn"], prediction_probas[test_mask])
     f = plt.figure(figsize=(12,5))
     f.add_subplot(121)
     sns.lineplot(precisions, recalls)
+    plt.xlabel("Precision")
+    plt.ylabel("Recall")
     f.add_subplot(122)
     sns.lineplot(np.append(thresholds, 1.0), precisions)
     sns.lineplot(np.append(thresholds, 1.0), recalls)
+    plt.xlabel("Threshold")
+    plt.legend(["Precision", "Recall"])
     
-    # Print best threshold, precision, recall and confusion matrix
+    # Print best threshold and performance on the test set
+    print("--- Test set performance ---")
     print(f"Best threshold: {best_threshold}")
-    print(confusion_matrix(df_ERA.loc[test_mask, "Foehn"], predictions))
-    print(f'Accuracy: {accuracy_score(df_ERA.loc[test_mask, "Foehn"], predictions)}')
-    print(f'Precision: {precision_score(df_ERA.loc[test_mask, "Foehn"], predictions)}')
-    print(f'Recall: {recall_score(df_ERA.loc[test_mask, "Foehn"], predictions)}')
-    print(f'ROC-AUC: {roc_auc_score(df_ERA.loc[test_mask, "Foehn"], prediction_probas[:,1])}')
-    print(f'Log-Loss: {log_loss(df_ERA.loc[test_mask, "Foehn"], prediction_probas[:,1])}')
-    
-    
-    # Show 50 most important features
+    print(confusion_matrix(df.loc[test_mask, "Foehn"], predictions[test_mask]))
+    print(f'Accuracy: {accuracy_score(df.loc[test_mask, "Foehn"], predictions[test_mask])}')
+    print(f'Precision: {precision_score(df.loc[test_mask, "Foehn"], predictions[test_mask])}')
+    print(f'Recall: {recall_score(df.loc[test_mask, "Foehn"], predictions[test_mask])}')
+    print(f'ROC-AUC: {roc_auc_score(df.loc[test_mask, "Foehn"], prediction_probas[test_mask])}')
+    print(f'Log-Loss: {log_loss(df.loc[test_mask, "Foehn"], prediction_probas[test_mask])}')
+
+    # Show 20 most important features
     df_ERA_feature_importances = pd.DataFrame({"feature_name": feature_names, "importance": best_model.feature_importances_}).sort_values(by="importance", ascending=False).reset_index(drop=True)
-    display(df_ERA_feature_importances.head(50))
+    display(df_ERA_feature_importances.head(20))
     
     return df_ERA_feature_importances
 
--- src/02_analysis/plot_mean_foehn_conditions.py
+++ src/02_analysis/plot_mean_foehn_conditions.py
@@ -38,51 +38,54 @@ def create_vectorfield(grid_U, grid_V, variable, variable_lvl, unit, model, vmin
     Create vector field plot for U and V variables.
     '''
   
+    # Convert string labels to floats
     lats = [int(lat)/100.0 for lat in lats_labels]
     lons = [int(lon)/100.0 for lon in lons_labels]
 
+    # Create mapfigure and set title (if wanted)
     mf = Mapfigure(lon=np.array(lons), lat=np.array(lats))
-#     fig = plt.figure(figsize=(16,9))
-#     plt.rcParams.update({'font.size': 20})
+    mf.drawmap(nbrem=1, nbrep=1)
 
+    # Calculate norm of U and V vectors
     grid_ges = np.sqrt(grid_U**2+grid_V**2)
 
+    # Set correct colorbar intervals in plot
     norm = matplotlib.colors.Normalize()
     norm.autoscale(grid_ges.flatten())
     cm = plt.cm.get_cmap('rocket_r')
-
     sm = plt.cm.ScalarMappable(cmap=cm)
     sm.set_array(grid_ges.flatten())
-
-    qui = plt.quiver(lons, lats, grid_U, grid_V, scale_units="xy", color=cm(norm(grid_ges.flatten())))
     
-    levels= np.arange(vmin, vmax, 1)
-    ticks= levels
+    # Make vector plot
+    plt.quiver(lons, lats, grid_U, grid_V, scale_units="xy", color=cm(norm(grid_ges.flatten())))
+    
+    # Format colorbar nicely
+    ticks = np.arange(vmin, vmax, 1)
     cbar = plt.colorbar(sm,
-                        boundaries=levels,
+                        boundaries=ticks,
                         ticks=ticks,
                         extend="both", fraction=0.0188, pad=0.03)
-    
     cbar.set_label(unit, rotation=90, labelpad=10)
 
+    # Plot the feature importances on the contour plot
     for i in df_importances.index:
         plt.plot(df_importances.loc[i, "lon1"]*0.01,
-                    df_importances.loc[i, "lat1"]*0.01,
-                    "bx",
-                    markersize=12,
-                    alpha=df_importances.loc[i, "importance"]/df_importances["importance"].max(),
-                    mew=4)
+                 df_importances.loc[i, "lat1"]*0.01,
+                 "bx",
+                 markersize=12*df_importances.loc[i, "importance"]/df_importances["importance"].max(),
+                 mew=4)
 
-    mf.drawmap(nbrem=1, nbrep=1)
-    #Plot Foehn location (Altdorf 8.64441, 46.88042, Piotta: 8.6833, 46.5167, Lugano: 8.96004, 46.01008)
+    
+    
+    # Plot station location (Altdorf 8.64441, 46.88042, Lugano: 8.96004, 46.01008)
     if location == "ALT":
         plt.plot(8.64441, 46.88042, 'o', color="#00FF00",markersize=8)
     elif location == "LUG":
         plt.plot(8.96004, 46.01008, 'o', color="#00FF00",markersize=8)
     
-
-#     plt.savefig(f'/home/chmony/Documents/Results/newgradient/weathermap_{variable}_{variable_lvl}_{model}.pdf', bbox_inches='tight', dpi=200)
-#     print(f"Saved figure at: /home/chmony/Documents/Results/newgradient/weathermap_{variable}_{variable_lvl}_{model}.pdf'")
+    # Uncomment if you want to save the figure
+    # plt.savefig(f'weathermap_{variable}_{variable_lvl}_{model}.pdf', bbox_inches='tight', dpi=200)
+    # print(f"Saved figure at: weathermap_{variable}_{variable_lvl}_{model}.pdf")
 
 def generate_coordinates_from_feature_label(df_features, variable):
     '''
@@ -112,20 +115,22 @@ def generate_coordinates_from_feature_label(df_features, variable):
         df_importances["importance"] = importance_list
         return df_importances
     
-# Obtain ERA-Interim mean grid configurations
+    
 def create_contour(grid, variable, variable_lvl, unit, model, vmin, vmax, lats_labels, lons_labels, df_importances, location):
     '''
     Create contour plot for a specified variable at a given pressure level.
     '''
-  
+    
+    # Convert string labels to floats
     lats = [int(lat)/100.0 for lat in lats_labels]
     lons = [int(lon)/100.0 for lon in lons_labels]
 
+    # Create mapfigure and set title (if wanted)
     mf = Mapfigure(lon=np.array(lons), lat=np.array(lats))
-#     fig = plt.figure(figsize=(16,9))
-    plt.rcParams.update({'font.size': 20})
-#     plt.title(f"{variable} at {variable_lvl} hPa for {model} data")
+    mf.drawmap(nbrem=1, nbrep=1)
+    # plt.title(f"{variable} at {variable_lvl} hPa for {model} data")
     
+    # Set correct colorbar intervals in plot
     if vmax-vmin<=10.2:
         levels = np.arange(vmin, vmax, 0.5)
         ticks= levels[1::2]
@@ -135,55 +140,52 @@ def create_contour(grid, variable, variable_lvl, unit, model, vmin, vmax, lats_l
     else:
         levels= np.arange(vmin, vmax, 10)
         ticks= levels[1::2]
-        
-    cmap=plt.cm.get_cmap('rocket')
     
+    # Make contour plot
+    cmap=plt.cm.get_cmap('rocket')
     cnt = plt.contourf(lons, lats, grid, levels=levels, cmap=cmap, vmin=vmin, vmax=vmax)
-#     cnt_l =  plt.contour(lons, lats, grid, levels=levels, colors="w", linewidths=0.001, vmin=vmin, vmax=vmax)
-#     plt.clabel(cnt_l, inline=0, fontsize=18, fmt="%1.1f",  levels= cnt_l.levels[1:-1:2])
+    
+    # If no white lines between levels in conotour plot are wanted, uncomment this
+    # for c in cnt.collections:
+    #     c.set_edgecolor("face")
+    
+    # Format colorbar nicely
     m = plt.cm.ScalarMappable(cmap=cmap)
     m.set_array(grid)
     m.set_clim(vmin, vmax)
- 
     cbar = plt.colorbar(m,
                         boundaries=levels,
                         ticks=ticks,
                         extend="both", fraction=0.0188, pad=0.03)
     cbar.set_label(unit, rotation=90, labelpad=10)
 
-#     for c in cnt.collections:
-# #         c.set_linewidth(0.0001)
-#         c.set_edgecolor("face")
-       
-
-
-    if variable == "DELTAPHI":
-        for i in range(len(df_importances.index)):
+    # Plot the feature importances on the contour plot
+    if variable == "DELTAPHI":  # In case of stability, plot a cross
+        for i in df_importances.index:
             plt.plot(df_importances.loc[i, "lon1"]*0.01,
-                    df_importances.loc[i, "lat1"]*0.01,
-                    "bx",
-                    markersize=12,
-                    alpha=df_importances.loc[i, "importance"]/df_importances["importance"].max(),
-                    mew=4)
+                     df_importances.loc[i, "lat1"]*0.01,
+                     "bx",
+                     markersize=12*df_importances.loc[i, "importance"]/df_importances["importance"].max(),
+                     alpha=df_importances.loc[i, "importance"]/df_importances["importance"].max(),
+                     mew=4)
 
-    else:
-        for i in range(len(df_importances.index)):
+    else:  # Plot lines in all other cases
+        for i in df_importances.index:
             plt.plot([df_importances.loc[i, "lon1"]*0.01, df_importances.loc[i, "lon2"]*0.01],
-                   [df_importances.loc[i, "lat1"]*0.01, df_importances.loc[i, "lat2"]*0.01],
-                   linewidth=5*df_importances.loc[i, "importance"]/df_importances["importance"].max(),
-                   c='b')
+                     [df_importances.loc[i, "lat1"]*0.01, df_importances.loc[i, "lat2"]*0.01],
+                     linewidth=5*df_importances.loc[i, "importance"]/df_importances["importance"].max(),
+                     c='b')
 
     
-    mf.drawmap(nbrem=1, nbrep=1)
-    #Plot Foehn location (Altdorf 8.64441, 46.88042, Piotta: 8.6833, 46.5167, Lugano: 8.96004, 46.01008)
+    # Plot station location (Altdorf 8.64441, 46.88042, Lugano: 8.96004, 46.01008)
     if location == "ALT":
         plt.plot(8.64441, 46.88042, 'o', color="#00FF00",markersize=8)
     elif location == "LUG":
         plt.plot(8.96004, 46.01008, 'o', color="#00FF00",markersize=8)
         
-   
-    #plt.savefig(f'/home/chmony/Documents/Results/newgradient/weathermap_{variable}_{variable_lvl}_{model}.pdf', bbox_inches='tight', dpi=200)
-    #print(f"Saved figure at: /home/chmony/Documents/Results/newgradient/weathermap_{variable}_{variable_lvl}_{model}.pdf'")
+    # Uncomment if you want to save the figure
+    # plt.savefig(f'weathermap_{variable}_{variable_lvl}_{model}.pdf', bbox_inches='tight', dpi=200)
+    # print(f"Saved figure at: weathermap_{variable}_{variable_lvl}_{model}.pdf")
 
     
 def plot_mean_foehn_condition_for_one_model(variable, variable_lvl, unit, model, vmin, vmax, df, foehn, lats_labels, lons_labels, df_importances,location):
