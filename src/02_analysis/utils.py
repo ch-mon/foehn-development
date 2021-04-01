a1754b3bd1f0352473bb890e4dbe7aac076fe84d
Chris Mony <chmony@student.ethz.ch>
2021-04-01 13:48:12 +0200

    Added documentation to 02_analysis/utils.py

--- src/02_analysis/utils.py
+++ src/02_analysis/utils.py
@@ -1,17 +1,18 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
+# Import constants from constants.py file in directory above
+import sys
+sys.path.append("..")
+from constants import *
 
-# Month abbreviation
-MONTH_NAMES = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-
-# Get base directory which is two levels up
-BASE_DIR = os.path.dirname(os.path.dirname(os.getcwd()))
 
 def calc_pot_temp(T, p):
-    '''
+    """
     Calculate the potential temperature at a given pressure level.
-    '''
+    @param T: Temperature array (in Kelvin)
+    @param p: Pressure array (in hPa)
+    """
     p_0 = 1000.0
     c_p = 1005.0
     R = 287.0
@@ -19,12 +20,17 @@ def calc_pot_temp(T, p):
     pot_temp = (T)*(p_0/p)**(R/c_p)
     return pot_temp
 
+
 def load_CESM(filepath, LATS_CESM_STRING, LONS_CESM_STRING):
-    '''
+    """
     Load a before created CESM file (present or future climate), append stability and potential temperature features and return full dataframe.
-    '''
+    @param filepath: filepath to CESMp or CESMf file
+    @param LATS_CESM_STRING: CESM latitude coordinates
+    @param LONS_CESM_STRING: CESM longitude coordinates
+    @return: Preprocessed CESMp or CESMf file
+    """
     df_CESM = pd.read_csv(filepath)
-    df_CESM["date"] = pd.to_datetime(df_CESM["date"], format="%Y-%m-%dT%H:%M:00.000000Z") # Due to Dataiku date format, evtl adjust this
+    df_CESM["date"] = pd.to_datetime(df_CESM["date"], format="%Y-%m-%dT%H:%M:00.000000Z")  # Due to Dataiku date format, evtl adjust this
     
     # Calculate and append potential temperature features
     df_T= df_CESM.filter(regex=("T\w+900")).add_prefix("PHI")
@@ -37,46 +43,54 @@ def load_CESM(filepath, LATS_CESM_STRING, LONS_CESM_STRING):
     df_CESM = pd.concat([df_CESM, df_PHIT_900, df_PHIT_850, df_PHIT_700], axis=1)
     
     # Calculate and append the stability features
-    df_CESM_stability = calculate_stability(df = df_CESM, lats = LATS_CESM_STRING, lons = LONS_CESM_STRING)
+    df_CESM_stability = calculate_stability(df=df_CESM, lats=LATS_CESM_STRING, lons=LONS_CESM_STRING)
     df_CESM = pd.concat([df_CESM, df_CESM_stability], axis=1)
 
-    # Return dataframe with all neceassary basic features
+    # Return dataframe with all necessary basic features
     return df_CESM
 
 
 def calculate_stability(df, lats, lons):
-    '''
-    Calculate the stability for 700--900 and 850--900 hPa layers from temperature features for all avaliable grid points.
-    '''
+    """
+    Calculate the stability for 700--900 and 850--900 hPa layers from temperature features for all available grid points in lats and lons.
+    @param df: ERAI or CESM dataframe
+    @param lats: CESM latitude coordinates
+    @param lons: CESM longitude coordinates
+    @return: Stability variable dataframe
+    """
     
     stability_dict = {}
-    
+    # Loop over all coordinates
     for lat in lats:
         for lon in lons:
-            try:
-                stability_dict[f"DELTAPHI_{lat}_{lon}_700"] = (df[f"PHIT_{lat}_{lon}_700"] - df[f"PHIT_{lat}_{lon}_900"]).values
-            except:
-                # Might happen since not all features are avaliable in CESM
-                pass
-
-            try:
-                stability_dict[f"DELTAPHI_{lat}_{lon}_850"] = (df[f"PHIT_{lat}_{lon}_850"] - df[f"PHIT_{lat}_{lon}_900"]).values
-            except:
-                # Might happen since not all features are avaliable in CESM
-                pass
+            for level in ["700", "850"]:
+                try:
+                    stability_dict[f"DELTAPHI_{lat}_{lon}_{level}"] = (df[f"PHIT_{lat}_{lon}_{level}"] - df[f"PHIT_{lat}_{lon}_900"]).values
+                except:
+                    # Might happen since not all features are available in CESM
+                    pass
     
     return pd.DataFrame(stability_dict) 
 
+
 def save_figure(name):
+    """
+    Save a PDF figure to figures folder.
+    @param name: Name of the figure
+    """
     filepath = os.path.join(BASE_DIR, f'figures/{name}.pdf')
     plt.savefig(filepath, bbox_inches='tight', dpi=200)
     print(f'Saved figure at: {filepath}')
     
     
 def calculate_horizontal_feature_differences(df, variable, pressure_levels):
-    '''
+    """
     Calculate the horizontal potential temperature differences for a given variable and pressure levels.
-    '''
+    @param df: ERAI or CESM dataframe
+    @param variable: Variable for which to calculate the horizontal difference
+    @param pressure_levels: For which pressure levels to calculate the horizontal differences
+    @return: Dataframe with horizontal differences
+    """
     
     df_variable = df.filter(regex=(f"{variable}_\w+"))
     
@@ -93,6 +107,7 @@ def calculate_horizontal_feature_differences(df, variable, pressure_levels):
                 feature_dict[f"diff_{col1}_{col2}"] = (df_level.loc[:, col1] - df_level.loc[:, col2]).values
 
     return pd.DataFrame(feature_dict)
+
 
 def generate_reduced_features_on_CESM(feature_to_generate, df_CESM):
     """
